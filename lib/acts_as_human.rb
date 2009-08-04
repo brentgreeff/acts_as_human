b7a9e050990457c98c0add6ca7eca4a41e274530
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2009-08-04 16:29:25 +0100

    Acts as human now takes optional 'last_name_required' default is true.

--- lib/acts_as_human.rb
+++ lib/acts_as_human.rb
@@ -1,24 +1,27 @@
 module ActsAs
   module Human
-    mattr_accessor :acceptable_name, :bad_name_message, :require_last_name
+    mattr_accessor :acceptable_name, :bad_name_message
     
     self.acceptable_name    = /\A[^[:cntrl:]\\<>\/&]*\z/
     self.bad_name_message   = "some characters in your name are not allowed".freeze
     
-    def acts_as_human(options={})
-      if options.has_key? :require_last_name
-        self.require_last_name = options[:require_last_name]
-      else
-        self.require_last_name = true
-      end
-      extend ClassMethods
-      include InstanceMethods
+    def self.included(base)
+      base.send :extend, ClassMethods
     end
     
     module ClassMethods
       
-      def self.extended(base)
-        base.class_eval do
+      def acts_as_human(options={})
+        cattr_accessor :require_last_name
+        
+        if options.has_key? :require_last_name
+          @require_last_name = options[:require_last_name]
+        else
+          @require_last_name = true
+        end
+        send :include, InstanceMethods
+
+        class_eval do
           validates_presence_of       :first_name, :message => 'first name is required'
           validates_length_of         :first_name, :maximum => 40,
                                       :message => 'first name is too long (max 40 characters)'
@@ -29,7 +32,7 @@ module ActsAs
                                       :message => 'middle names are too long (max 40 characters)'
           validates_as_person_name    :middle_names, :allow_nil => true
           
-          validates_presence_of       :last_name, :if => Proc.new {ActsAs::Human.require_last_name},
+          validates_presence_of       :last_name, :if => Proc.new { @require_last_name },
                                       :message => 'last name is required'
           validates_length_of         :last_name, :maximum => 40,
                                       :allow_blank => true,
@@ -37,6 +40,7 @@ module ActsAs
           validates_as_person_name    :last_name
         end
       end
+      
     end
     
     module InstanceMethods
--- rails/init.rb
+++ rails/init.rb
@@ -1,4 +1,4 @@
 require 'acts_as_human/validations'
 require 'acts_as_human'
 
-ActiveRecord::Base.send(:extend, ActsAs::Human)
+ActiveRecord::Base.send(:include, ActsAs::Human)
