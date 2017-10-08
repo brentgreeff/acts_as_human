482be776770d1a4c26a2b6f2e2eb5d1ea2c92781
Brent <brentgreeff@gmail.com>
2017-10-09 01:21:53 +0700

    Remove whitespace instance_methods and validations

--- lib/acts_as_human/instance_methods.rb
+++ lib/acts_as_human/instance_methods.rb
@@ -1,27 +1,27 @@
 module ActsAs
   module Human
-    
+
     module InstanceMethods
       def full_name
         return '' if first_name.blank? and last_name.blank?
-      
+
         return "#{first_name} #{last_name}" if middle_names.blank?
         return "#{first_name} #{middle_names} #{last_name}"
       end
-      
+
       def full_name=(names)
         names_array = names.titlecase.split
-        
+
         self.first_name = names_array.first
         return if names_array.size < 2
-        
+
         self.last_name = get_last_name(names_array)
-        
+
         assign_middle_names(names_array)
       end
-      
+
       private
-      
+
       def get_last_name(names_array)
         if names_array.size > 2 and names_array[-2].eql? 'Mc'
           surname = names_array.pop
@@ -29,7 +29,7 @@ module ActsAs
         end
         names_array.last
       end
-      
+
       def assign_middle_names(names_array)
         if names_array.size > 2
           self.middle_names = get_middle_names(names_array)
@@ -37,11 +37,10 @@ module ActsAs
           self.middle_names = nil
         end
       end
-      
+
       def get_middle_names(names_array)
         names_array[1..(names_array.size-2)].join(' ')
       end
     end
   end
 end
-
--- lib/acts_as_human/validations.rb
+++ lib/acts_as_human/validations.rb
@@ -1,7 +1,7 @@
 module ActiveRecord
   module Validations
     module ClassMethods
-	    
+
 	    def validates_as_person_name(*attr_names)
 	      config = {
 	        :with => ActsAs::Human.acceptable_name,
@@ -9,14 +9,14 @@ module ActiveRecord
 	      }
 	      check_format(attr_names, config)
 	    end
-			
+
 			private
-			
+
 			def check_format(attr_names, config)
 			  config.update(attr_names.pop) if attr_names.last.is_a?(Hash)
 		    validates_format_of attr_names, config
 			end
-			
+
     end
   end
 end
