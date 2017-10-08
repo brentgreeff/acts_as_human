da7d8a045c9b877a94f476db4353921a08bf5750
Brent <brentgreeff@gmail.com>
2017-10-09 01:18:07 +0700

    Change to new hash syntax in class_methods

--- lib/acts_as_human/class_methods.rb
+++ lib/acts_as_human/class_methods.rb
@@ -2,8 +2,8 @@ module ActsAs
   module Human
     mattr_accessor :acceptable_name, :bad_name_message
 
-    self.acceptable_name    = /\A[^[:cntrl:]\\<>\/&]*\z/
-    self.bad_name_message   = "some characters in your name are not allowed".freeze
+    self.acceptable_name = /\A[^[:cntrl:]\\<>\/&]*\z/
+    self.bad_name_message = "some characters in your name are not allowed".freeze
 
     def self.included(base)
       base.send :extend, ClassMethods
@@ -22,24 +22,24 @@ module ActsAs
         send :include, InstanceMethods
 
         class_eval do
-          validates_presence_of :first_name, :message => 'first name is required'
-          validates_length_of :first_name, :maximum => 40,
-            :message => 'first name is too long (max 40 characters)'
+          validates_presence_of :first_name, message: 'first name is required'
+          validates_length_of :first_name, maximum: 40,
+            message: 'first name is too long (max 40 characters)'
 
           validates_as_person_name :first_name
 
-          validates_length_of :middle_names, :maximum => 40,
-            :allow_blank => true,
-            :message => 'middle names are too long (max 40 characters)'
+          validates_length_of :middle_names, maximum: 40,
+            allow_blank: true,
+            message: 'middle names are too long (max 40 characters)'
 
-          validates_as_person_name :middle_names, :allow_nil => true
+          validates_as_person_name :middle_names, allow_nil: true
 
-          validates_presence_of :last_name, :if => Proc.new { @require_last_name },
-            :message => 'last name is required'
+          validates_presence_of :last_name, if: Proc.new { @require_last_name },
+            message: 'last name is required'
 
-          validates_length_of :last_name, :maximum => 40,
-            :allow_blank => true,
-            :message => 'last name is too long (max 40 characters)'
+          validates_length_of :last_name, maximum: 40,
+            allow_blank: true,
+            message: 'last name is too long (max 40 characters)'
 
           validates_as_person_name :last_name
         end
